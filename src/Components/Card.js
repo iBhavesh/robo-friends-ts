a0fc8d47f797411ba96d93f785d95bde36491c62
Bhavesh Sharma <34792643+iBhavesh@users.noreply.github.com>
2020-06-13 17:21:21 +0530

    Add fetch, refactor code

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,13 @@
-import React from 'react';
-
-const Card = ({name, email, id}) => (
-    <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
-        <img src ={`https://robohash.org/${id}?size=200x200`} alt={name} />
-        <div>
-            <h2>{name}</h2>
-            <p>{email}</p>
-        </div>
+import React from "react";
+import "tachyons";
+const Card = ({ name, email, id }) => (
+  <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow">
+    <img src={`https://robohash.org/${id}?size=200x200`} alt={name} />
+    <div>
+      <h2>{name}</h2>
+      <p>{email}</p>
     </div>
+  </div>
 );
 
 export default Card;
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,40 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import CardList from "../Components/CardList";
 import SearchBox from "../Components/SearchBox";
-import { robots } from "../robots";
-
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: "",
     };
     this.onSearchChange = this.onSearchChange.bind(this);
   }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
+
   onSearchChange(event) {
     this.setState({ searchField: event.target.value });
-    console.log("event value:", event.target.value);
   }
 
   render() {
-    console.log("state searchField:", this.state.searchField);
     const filteredRobots = this.state.robots.filter((robot) =>
       robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
     );
-    console.log(filteredRobots);
-    return (
-      <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox
-          searchField={this.state.searchField}
-          searchChange={this.onSearchChange}
-        />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox
+            searchField={this.state.searchField}
+            searchChange={this.onSearchChange}
+          />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
